f3fbb6d1d76c0f56c4aa9e8692e3de37ace6bd7a
Alpha Barry <alpha.mt.barry@gmail.com>
2021-02-20 15:49:57 +0400

    Formatting

--- week1/percolation/Percolation.java
+++ week1/percolation/Percolation.java
@@ -7,127 +7,127 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private final WeightedQuickUnionUF grid;
-    private final int n;
-    private final boolean[] openSites;
-    private int openSitesCount;
+  private final WeightedQuickUnionUF grid;
+  private final int n;
+  private final boolean[] openSites;
+  private int openSitesCount;
+  private final int virtualTop;
+  private final int virtualBottom;
 
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {
-        this.n = n;
-        validate(n);
-        grid = new WeightedQuickUnionUF(n * n);
-        openSites = new boolean[n * n];
-        openSitesCount = 0;
-        createVirtualSites();
+  // creates n-by-n grid, with all sites initially blocked
+  public Percolation(int n) {
+    this.n = n;
+    validate(n);
+    int nSquared = n * n;
+    // add two virtual sites
+    grid = new WeightedQuickUnionUF(nSquared + 2);
+    openSites = new boolean[nSquared];
+    openSitesCount = 0;
+    virtualTop = nSquared;
+    virtualBottom = 1 + nSquared;
+  }
+
+  /**
+   * Checks if the given row/col is in range
+   */
+  private void validate(int coordinate) {
+    if (coordinate <= 0 || coordinate > n) {
+      throw new IllegalArgumentException("Out of range");
+    }
+  }
+
+  /**
+   * Returns the index in the grid
+   */
+  private int getIndexOf(int row, int col) {
+    validate(row);
+    validate(col);
+    return (row - 1) * n + (col - 1);
+  }
+
+  private void openNeighbor(int site, int neighbor) {
+    if (openSites[neighbor]) {
+      grid.union(site, neighbor);
+    }
+  }
+
+  // opens the site (row, col) if it is not open already
+  public void open(int row, int col) {
+    int site = getIndexOf(row, col);
+    if (!openSites[site]) {
+      openSitesCount += 1;
+      openSites[site] = true;
+
+      if (row > 1) {
+        openNeighbor(site, getIndexOf(row - 1, col));
+      }
+      if (row < n) {
+        openNeighbor(site, getIndexOf(row + 1, col));
+      }
+
+      if (col > 1) {
+        openNeighbor(site, getIndexOf(row, col - 1));
+      }
+      if (col < n) {
+        openNeighbor(site, getIndexOf(row, col + 1));
+      }
+
+      // when we open a node in the top or bottom row connect it to the virtual sites
+      if (row == 1) {
+        grid.union(virtualTop, site);
+      }
+      if (row == n) {
+        grid.union(virtualBottom, site);
+      }
+    }
+  }
+
+  private boolean isOpenAtTop() {
+    // check if there is an open site at the top
+    boolean openAtTop = false;
+    for (int i = 0; i < n; i++) {
+      openAtTop = openAtTop || openSites[i];
+      if (openAtTop) {
+        break;
+      }
     }
 
-    private void createVirtualSites() {
-        // connect first row nodes
-        int first = getIndexOf(1, 1); // 0
-        for (int col = 2; col <= n; col++) {
-            grid.union(first, getIndexOf(1, col));
-        }
+    return openAtTop;
+  }
 
-        // connect last row nodes
-        int lastRowFirstNode = getIndexOf(n, 1);
-        for (int col = 2; col <= n; col++) {
-            grid.union(lastRowFirstNode, getIndexOf(n, col));
-        }
+  // is the site (row, col) open?
+  public boolean isOpen(int row, int col) {
+    return openSites[getIndexOf(row, col)];
+  }
+
+  // is the site (row, col) full?
+  public boolean isFull(int row, int col) {
+    int site = getIndexOf(row, col);
+
+    // stop if the site is not opened
+    if (!openSites[site]) {
+      return false;
     }
 
-    /**
-     * Checks if the given row/col is in range
-     */
-    private void validate(int coordinate) {
-        if (coordinate <= 0 || coordinate > n) {
-            throw new IllegalArgumentException("Out of range");
-        }
+    if (!isOpenAtTop()) {
+      return false;
     }
 
-    /**
-     * Returns the index in the grid
-     */
-    private int getIndexOf(int row, int col) {
-        validate(row);
-        validate(col);
-        return (row - 1) * n + (col - 1);
+    // check if we have have connection to the top
+    if (grid.find(virtualTop) != grid.find(site)) {
+      return false;
     }
 
-    private void openNeighbor(int site, int neighbor) {
-        if (openSites[neighbor]) {
-            grid.union(site, neighbor);
-        }
-    }
+    return true;
+  }
 
-    // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {
-        int site = getIndexOf(row, col);
-        if (!openSites[site]) {
-            openSitesCount += 1;
-            openSites[site] = true;
+  // returns the number of open sites
+  public int numberOfOpenSites() {
+    return openSitesCount;
+  }
 
-            if (row > 1) {
-                openNeighbor(site, getIndexOf(row - 1, col));
-            }
-            if (row < n) {
-                openNeighbor(site, getIndexOf(row + 1, col));
-            }
-
-            if (col > 1) {
-                openNeighbor(site, getIndexOf(row, col - 1));
-            }
-            if (col < n) {
-                openNeighbor(site, getIndexOf(row, col + 1));
-            }
-        }
-    }
-
-    private boolean isOpenAtTop() {
-        // check if there is an open site at the top
-        boolean openAtTop = false;
-        for (int i = 0; i < n; i++) {
-            openAtTop = openAtTop || openSites[i];
-            if (openAtTop) {
-                break;
-            }
-        }
-
-        return openAtTop;
-    }
-
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
-        return openSites[getIndexOf(row, col)];
-    }
-
-    // is the site (row, col) full?
-    public boolean isFull(int row, int col) {
-        int site = getIndexOf(row, col);
-
-        // stop if the site is not opened
-        if (!openSites[site]) {
-            return false;
-        }
-
-        if (!isOpenAtTop()) {
-            return false;
-        }
-
-        // finally check if we have have connection
-        return grid.find(0) == grid.find(site);
-    }
-
-    // returns the number of open sites
-    public int numberOfOpenSites() {
-        return openSitesCount;
-    }
-
-    // does the system percolate?
-    public boolean percolates() {
-        int last = getIndexOf(n, n);
-        return isOpenAtTop() && (grid.find(0) == grid.find(last));
-    }
-
-
+  // does the system percolate?
+  public boolean percolates() {
+    return isOpenAtTop() && (grid.find(virtualTop) == grid.find(virtualBottom));
+  }
 }
--- week1/percolation/PercolationStats.java
+++ week1/percolation/PercolationStats.java
@@ -9,73 +9,73 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private static final double ONE_96 = 1.96;
-    private final int n;
-    private final int trials;
-    private double[] results;
+  private static final double ONE_96 = 1.96;
+  private final int n;
+  private final int trials;
+  private double[] results;
 
-    // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) {
-        if (Integer.min(n, trials) <= 0) {
-            throw new IllegalArgumentException("arguments are negative");
-        }
-
-        this.n = n;
-        this.trials = trials;
-
-        runSimulation();
+  // perform independent trials on an n-by-n grid
+  public PercolationStats(int n, int trials) {
+    if (Integer.min(n, trials) <= 0) {
+      throw new IllegalArgumentException("arguments are negative");
     }
 
-    private void printResults() {
-        StdOut.println("mean = " + mean());
-        StdOut.println("stddev = " + stddev());
-        StdOut.println("95% confidence interval = [" + confidenceLo() + ", " + confidenceHi() + "]");
+    this.n = n;
+    this.trials = trials;
+
+    runSimulation();
+  }
+
+  private void printResults() {
+    StdOut.println("mean = " + mean());
+    StdOut.println("stddev = " + stddev());
+    StdOut.println("95% confidence interval = [" + confidenceLo() + ", " + confidenceHi() + "]");
+  }
+
+  private void runSimulation() {
+    results = new double[trials];
+    for (int i = 0; i < trials; i++) {
+      Percolation p = new Percolation(n);
+      while (!p.percolates()) {
+        int row = StdRandom.uniform(1, n + 1);
+        int col = StdRandom.uniform(1, n + 1);
+        p.open(row, col);
+      }
+      results[i] = ((double) p.numberOfOpenSites()) / (n * n);
     }
+  }
 
-    private void runSimulation() {
-        results = new double[trials];
-        for (int i = 0; i < trials; i++) {
-            Percolation p = new Percolation(n);
-            while (!p.percolates()) {
-                int row = StdRandom.uniform(1, n + 1);
-                int col = StdRandom.uniform(1, n + 1);
-                p.open(row, col);
-            }
-            results[i] = ((double) p.numberOfOpenSites()) / (n * n);
-        }
-    }
+  // sample mean of percolation threshold
+  public double mean() {
+    return StdStats.mean(results);
+  }
 
-    // sample mean of percolation threshold
-    public double mean() {
-        return StdStats.mean(results);
-    }
+  // sample standard deviation of percolation threshold
+  public double stddev() {
+    return StdStats.stddev(results);
+  }
 
-    // sample standard deviation of percolation threshold
-    public double stddev() {
-        return StdStats.stddev(results);
-    }
+  // low endpoint of 95% confidence interval
+  public double confidenceLo() {
+    double mean = mean();
+    double stdDev = stddev();
 
-    // low endpoint of 95% confidence interval
-    public double confidenceLo() {
-        double mean = mean();
-        double stdDev = stddev();
+    return mean - ((ONE_96 * stdDev) / Math.sqrt((double) trials));
+  }
 
-        return mean - ((ONE_96 * stdDev) / Math.sqrt((double) trials));
-    }
+  // high endpoint of 95% confidence interval
+  public double confidenceHi() {
+    double mean = mean();
+    double stdDev = stddev();
 
-    // high endpoint of 95% confidence interval
-    public double confidenceHi() {
-        double mean = mean();
-        double stdDev = stddev();
+    return mean + ((ONE_96 * stdDev) / Math.sqrt((double) trials));
+  }
 
-        return mean + ((ONE_96 * stdDev) / Math.sqrt((double) trials));
-    }
+  public static void main(String[] args) {
+    int n = Integer.parseInt(args[0]);
+    int trials = Integer.parseInt(args[1]);
 
-    public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        int trials = Integer.parseInt(args[1]);
-
-        PercolationStats simulation = new PercolationStats(n, trials);
-        simulation.printResults();
-    }
+    PercolationStats simulation = new PercolationStats(n, trials);
+    simulation.printResults();
+  }
 }
